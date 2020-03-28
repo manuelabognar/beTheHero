f04c8a9a5068a4cb082fcd3f21de9c708dc8379d
Manuela Bognar <manuelabcamara@gmail.com>
2020-03-27 23:58:38 -0300

    Routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>oi!</Text>
-    </View>
+    <Routes />
   );
 }
 
